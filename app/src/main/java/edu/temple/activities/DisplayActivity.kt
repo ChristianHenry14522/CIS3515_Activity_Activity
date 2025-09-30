1de696cfb2a6eb08e3c8b397682a2c5f7ca3e7c5
itsch <tuq14522@temple.edu>
2025-09-30 12:15:15 -0400

    September 30th class activity

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -1,19 +1,21 @@
 package edu.temple.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class DisplayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display)
 
-        // TODO Step 3: Extract transferred value and use for lyricsDisplayView text size
-        with (findViewById<TextView>(R.id.lyricsDisplayTextView)) {
+        val selectedSize = intent.getIntExtra("TEXT_SIZE", 20)
 
+        with(findViewById<TextView>(R.id.lyricsDisplayTextView)) {
+
+            // this.textSize = selectedSize.toFloat()
+            setTextSize(TypedValue.COMPLEX_UNIT_SP, selectedSize.toFloat() )
         }
-
-
     }
 }
